07b79033587122d4ebae0ad57a0f9b99d16915bf
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-09-23 21:19:39 +0100

    Resolving freelancer exercise

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,28 @@
 defmodule FreelancerRates do
+  @daily_rate 8
+  @month_billable_days 22
+  @decimal_places 1
+
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    (hourly_rate * @daily_rate * 100 / 100)
+    |> Float.round(@decimal_places)
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount - before_discount * discount / 100
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    (@month_billable_days * daily_rate(hourly_rate))
+    |> apply_discount(discount)
+    |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    daily_rate_with_discount =
+      daily_rate(hourly_rate)
+      |> apply_discount(discount)
+
+    (budget / daily_rate_with_discount) |> Float.floor(@decimal_places)
   end
 end
