889a89f5bfe771c464d8fcb4e4b0af4046711919
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-09-24 18:05:59 +0100

    Use cond macro in the resolution

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,39 +1,24 @@
 defmodule LogLevel do
-  @log_level_codes %{
-    {0, false} => :trace,
-    {1, true} => :debug,
-    {1, false} => :debug,
-    {2, true} => :info,
-    {2, false} => :info,
-    {3, true} => :warning,
-    {3, false} => :warning,
-    {4, true} => :error,
-    {4, false} => :error,
-    {5, false} => :fatal
-  }
-
-  @teams_to_receive_alerts %{
-    {:error, false} => :ops,
-    {:error, true} => :ops,
-    {:fatal, false} => :ops,
-    {:fatal, true} => :ops,
-    {:unknown, true} => :dev1,
-    {:unknown, false} => :dev2
-  }
-
   def to_label(level, legacy?) do
-    case @log_level_codes[{level, legacy?}] do
-      nil -> :unknown
-      log_level -> log_level
+    cond do
+      level == 0 && legacy? == false -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 && legacy? == false -> :fatal
+      true -> :unknown
     end
   end
 
   def alert_recipient(level, legacy?) do
     log_level = to_label(level, legacy?)
 
-    case @teams_to_receive_alerts[{log_level, legacy?}] do
-      nil -> false
-      team -> team
+    cond do
+      log_level == :error || log_level == :fatal -> :ops
+      log_level == :unknown && legacy? == true -> :dev1
+      log_level == :unknown && legacy? == false -> :dev2
+      true -> false
     end
   end
 end
