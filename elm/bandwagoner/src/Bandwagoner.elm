e5f6605e0966a203119cc6e4deb5e8dd89b78fb3
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-10-22 15:13:09 +0100

    Fix records exercise

--- elm/bandwagoner/src/Bandwagoner.elm
+++ elm/bandwagoner/src/Bandwagoner.elm
@@ -1,22 +1,46 @@
 module Bandwagoner exposing (..)
 
--- TODO: please define a type alias for the 'Coach' record
--- TODO: please define the 'Stats' record
--- TODO: please define the 'Team' record
+
+type alias Coach =
+    { name : String
+    , formerPlayer : Bool
+    }
 
 
+type alias Stats =
+    { wins : Int
+    , losses : Int
+    }
+
+
+type alias Team =
+    { name : String
+    , coach : Coach
+    , stats : Stats
+    }
+
+
+replaceCoach : Coach -> Team -> Team
 replaceCoach newCoach team =
-    Debug.todo "Please implement this function"
+    { team | coach = newCoach }
 
 
+createTeam : String -> Stats -> Coach -> Team
 createTeam name stats coach =
-    Debug.todo "Please implement this function"
+    Team name coach stats
 
 
-
--- TODO: use an extensible record to accept any record that has a `stats: Stats` field
--- TODO: use pattern matching in the function parameter to get the `stats` field from the record
-
-
+rootForTeam : { team | stats : Stats } -> Bool
 rootForTeam team =
-    Debug.todo "Please implement this function, try to pattern match in the function parameter"
+    let
+        { stats } =
+            team
+
+        { wins, losses } =
+            stats
+    in
+    if wins > losses then
+        True
+
+    else
+        False
