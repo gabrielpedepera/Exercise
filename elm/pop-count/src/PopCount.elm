a278520a739985cf4140e5fc9d85eee1db4f3920
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-10-22 16:33:37 +0100

    Resolve popCount exercise

--- elm/pop-count/src/PopCount.elm
+++ elm/pop-count/src/PopCount.elm
@@ -1,6 +1,38 @@
 module PopCount exposing (eggCount)
 
 
+toBinaryAndCountOnes : Int -> ( List Int, Int )
+toBinaryAndCountOnes n =
+    if n == 0 then
+        ( [ 0 ], 0 )
+
+    else
+        let
+            convertAndCount x acc count =
+                if x == 0 then
+                    ( acc, count )
+
+                else
+                    let
+                        bit =
+                            x |> modBy 2
+
+                        newCount =
+                            if bit == 1 then
+                                count + 1
+
+                            else
+                                count
+                    in
+                    convertAndCount (x // 2) (bit :: acc) newCount
+        in
+        convertAndCount n [] 0
+
+
 eggCount : Int -> Int
 eggCount n =
-    Debug.todo "implement this function"
+    let
+        ( _, onesCount ) =
+            toBinaryAndCountOnes n
+    in
+    onesCount
