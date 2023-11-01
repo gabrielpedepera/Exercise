9ca4819659a4992a5254702416653949144c89b7
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-11-01 23:39:45 +0000

    Resolve string exercise

--- elm/squeaky-clean/src/SqueakyClean.elm
+++ elm/squeaky-clean/src/SqueakyClean.elm
@@ -1,26 +1,62 @@
 module SqueakyClean exposing (clean, clean1, clean2, clean3, clean4)
 
+import Char exposing (isDigit, toCode, toUpper)
+import String exposing (cons, filter, join, replace, split, uncons)
+
 
 clean1 : String -> String
 clean1 str =
-    Debug.todo "Please implement this function"
+    replace " " "_" str
 
 
 clean2 : String -> String
 clean2 str =
-    Debug.todo "Please implement this function"
+    str
+        |> clean1
+        |> replace "\n" "[CTRL]"
+        |> replace "\u{000D}" "[CTRL]"
+        |> replace "\t" "[CTRL]"
 
 
 clean3 : String -> String
 clean3 str =
-    Debug.todo "Please implement this function"
+    str
+        |> clean2
+        |> split "-"
+        |> List.indexedMap
+            (\i word ->
+                case uncons word of
+                    Nothing ->
+                        ""
+
+                    Just ( firstChar, restOfString ) ->
+                        if i > 0 then
+                            cons (toUpper firstChar) restOfString
+
+                        else
+                            word
+            )
+        |> join ""
 
 
 clean4 : String -> String
 clean4 str =
-    Debug.todo "Please implement this function"
+    str
+        |> clean3
+        |> filter (\char -> not (isDigit char))
+
+
+isGreekLetter : Char -> Bool
+isGreekLetter char =
+    let
+        code =
+            toCode char
+    in
+    code >= 0x03B1 && code <= 0x03C9
 
 
 clean : String -> String
 clean str =
-    Debug.todo "Please implement this function"
+    str
+        |> clean4
+        |> filter (\char -> not (isGreekLetter char))
