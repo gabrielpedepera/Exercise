cf2b64753d8d6398447cd8613560626b3ceed7a2
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-11-12 20:16:47 +0000

    Resolve recursion exercise

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,37 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
+  def today([]), do: nil
+
+  def today([today | _]) do
+    today
   end
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+  def increment_day_count([]), do: [1]
+
+  def increment_day_count([today | rest]) do
+    [today + 1 | rest]
   end
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+  def has_day_without_birds?([]), do: false
+
+  def has_day_without_birds?([today | rest]) do
+    case today do
+      0 -> true
+      _ -> has_day_without_birds?(rest)
+    end
   end
 
-  def total(list) do
-    # Please implement the total/1 function
+  def total([]), do: 0
+
+  def total([today | rest]) do
+    today + total(rest)
   end
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
+  def busy_days([]), do: 0
+
+  def busy_days([today | rest]) do
+    case today >= 5 do
+      true -> 1 + busy_days(rest)
+      false -> busy_days(rest)
+    end
   end
 end
