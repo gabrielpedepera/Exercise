cd372cac334d26dbcab88c7d47df462a08a5b571
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-11-12 20:25:45 +0000

    Refactor with inline functions

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,37 +1,18 @@
 defmodule BirdCount do
   def today([]), do: nil
-
-  def today([today | _]) do
-    today
-  end
+  def today([today | _]), do: today
 
   def increment_day_count([]), do: [1]
-
-  def increment_day_count([today | rest]) do
-    [today + 1 | rest]
-  end
+  def increment_day_count([today | rest]), do: [today + 1 | rest]
 
   def has_day_without_birds?([]), do: false
-
-  def has_day_without_birds?([today | rest]) do
-    case today do
-      0 -> true
-      _ -> has_day_without_birds?(rest)
-    end
-  end
+  def has_day_without_birds?([today | _]) when today == 0, do: true
+  def has_day_without_birds?([_ | rest]), do: has_day_without_birds?(rest)
 
   def total([]), do: 0
-
-  def total([today | rest]) do
-    today + total(rest)
-  end
+  def total([today | rest]), do: today + total(rest)
 
   def busy_days([]), do: 0
-
-  def busy_days([today | rest]) do
-    case today >= 5 do
-      true -> 1 + busy_days(rest)
-      false -> busy_days(rest)
-    end
-  end
+  def busy_days([today | rest]) when today >= 5, do: 1 + busy_days(rest)
+  def busy_days([_ | rest]), do: busy_days(rest)
 end
